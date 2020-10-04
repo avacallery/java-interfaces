d5bd99637758954cd3d6133d3f15a34de37def84
avacallery <avacallery@gmail.com>
2020-10-03 20:07:37 -0400

    constructor and setter injection

--- src/com/avacallery/Main.java
+++ src/com/avacallery/Main.java
@@ -4,6 +4,8 @@ package com.avacallery;
 public class Main {
 
     public static void main(String[] args) {
-
+        var calculator = new TaxCalculator2020(100_000);
+        // it is up to Main to inject implementations (ex: TaxReport(calculator))
+        var report = new TaxReport(calculator);
     }
 }
--- src/com/avacallery/TaxReport.java
+++ src/com/avacallery/TaxReport.java
@@ -4,6 +4,8 @@ public class TaxReport {
     // what state and behavior? We need to work with TaxCalculator
     private TaxCalculator calculator;
 
+    // we are injecting a dependency using the constructor
+    // who is going to inject this?
     public TaxReport(TaxCalculator calculator) {
         this.calculator = calculator;
     }
